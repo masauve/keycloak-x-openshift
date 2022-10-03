29e5397edf37e4094ff235edc0be02ff394a63f2
masauve <msauve@redhat.com>
2022-10-03 12:36:19 -0400

    gitignore

--- policies/target/classes/match-email-domain.js
+++
@@ -1,8 +0,0 @@
-var context = $evaluation.getContext();
-var identity = context.getIdentity();
-var attributes = identity.getAttributes();
-var email = attributes.getValue('email').asString(0);
-
-if (email.endsWith('@redhat.com')) {
-    $evaluation.grant();
-}
